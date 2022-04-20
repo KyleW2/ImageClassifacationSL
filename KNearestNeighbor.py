c50b4b1e9b8f03d5a42a94a7d5f0e04f0caa26bf
Kyle <Kyle.J.Weidner-1@ou.edu>
2022-04-20 01:44:34 -0500

    Working with all 50,000 instances

--- KNearestNeighbor.py
+++ KNearestNeighbor.py
@@ -53,27 +53,56 @@ if __name__ == "__main__":
 
         return dict
     
-    # Load batch 1
+    # Load batches
     batch1 = unpickle("data/data_batch_1")
+    batch2 = unpickle("data/data_batch_2")
+    batch3 = unpickle("data/data_batch_3")
+    batch4 = unpickle("data/data_batch_4")
+    batch5 = unpickle("data/data_batch_5")
     
     # Formatting data into a more digestable form
-    batch1_f = []
-    for i in range(0, 10000):
-                          # Casting all values TO and int from uint8
-        batch1_f.append( [numpy.array([int(x) for x in batch1[b'data'][i]]), batch1[b'labels'][i]] )
+    def format(batch: dict) -> list:
+        formatted = []
+        for i in range(0, 10000):
+            # Casting all values TO and int from uint8
+            formatted.append( [numpy.array([int(x) for x in batch[b'data'][i]]), batch[b'labels'][i]] )
+        
+        return formatted
 
+    """
+    batch1_f = format(batch1)
+    batch2_f = format(batch2)
+    batch3_f = format(batch3)
+    batch4_f = format(batch4)
+    batch5_f = format(batch5)
+    """
+
+    # Absolute gigabrain ram killing function
+    def frankenstein(b1: dict, b2, b3, b4, b5) -> list:
+        frankenstein = []
+        for i in range(0, 10000):
+            frankenstein.append( [numpy.array([int(x) for x in b1[b'data'][i]]), b1[b'labels'][i]] )
+            frankenstein.append( [numpy.array([int(x) for x in b2[b'data'][i]]), b2[b'labels'][i]] )
+            frankenstein.append( [numpy.array([int(x) for x in b3[b'data'][i]]), b3[b'labels'][i]] )
+            frankenstein.append( [numpy.array([int(x) for x in b4[b'data'][i]]), b4[b'labels'][i]] )
+            frankenstein.append( [numpy.array([int(x) for x in b5[b'data'][i]]), b5[b'labels'][i]] )
+        
+        return frankenstein
+    
+    print(">> Creating the unholy frankenstein")
+    allBatches = frankenstein(batch1, batch2, batch3, batch4, batch5)
+
+    print(">> Loading and formatting test data")
     # Load test data and format
     test = unpickle("data/test_batch")
-
-    test_f = []
-    for i in range(0, 10000):
-                        # Casting all values TO and int from uint8
-        test_f.append( [numpy.array([int(x) for x in test[b'data'][i]]), test[b'labels'][i]] )
+    test_f = format(test)
     
+    print(">> Creating KNN object")
     # Create KNN object
-    K3 = KNearestNeighbor(batch1_f)
+    K3 = KNearestNeighbor(allBatches)
     
     print(test_f[0][1], end = " | ") # ith label
     print(test_f[0][0]) # list of rgb values [r, r, r, .... g, b, b]
 
-    print(f"test_f[0] classified as {K3.classify(test_f[0][0], [1, 3, 10, 100, 1000])}")
+    print(">> Classifying test 0")
+    print(K3.classify(test_f[0][0], [1, 3, 10, 100, 1000]))
