fdda5916e629d6b26355179c4fbbd7b0cfdd8ff6
Kyle <Kyle.J.Weidner-1@ou.edu>
2022-04-20 01:28:36 -0500

    Changed classify function to take in list of k's

--- KNearestNeighbor.py
+++ KNearestNeighbor.py
@@ -2,9 +2,8 @@ import numpy
 import pickle
 
 class KNearestNeighbor:
-    def __init__(self, data: list, k: int) -> None:
+    def __init__(self, data: list) -> None:
         self.data = data
-        self.k = k
 
     # Find the distance between two vectors
     def eulideanDistance(self, l1: list, l2: list) -> float:
@@ -26,13 +25,23 @@ class KNearestNeighbor:
         return distances
     
     # Classify a vector
-    def classify(self, p: list) -> int:
+    def classify(self, p: list, k: list) -> dict:
         distances = self.calculateDistances(p)
+        
+        c = {}
+        for i in range(0, len(k)):
+            c[k[i]] = self._classify(p, k[i], distances)
 
+        # Dictionary of {k: label}
+        return c
+    
+    # Helper function that does the actual calculations
+    # Allows for multiple K values to test with
+    def _classify(self, p: list, k: int, distances: list) -> float:
         sum = 0
-        for i in range(0, self.k):
+        for i in range(0, k):
             sum += distances[i][0][1]
-        sum = sum / self.k
+        sum = sum / k
 
         return sum
 
@@ -46,22 +55,12 @@ if __name__ == "__main__":
     
     # Load batch 1
     batch1 = unpickle("data/data_batch_1")
-
-    """ 
-    # Printing the first 10 for reading
-    for i in range(0, 10):
-        print(batch1[b'labels'][i], end = " | ") # ith label
-        print(batch1[b'data'][i]) # list of rgb values [r, r, r, .... g, b, b]
-    """
     
     # Formatting data into a more digestable form
     batch1_f = []
     for i in range(0, 10000):
                           # Casting all values TO and int from uint8
         batch1_f.append( [numpy.array([int(x) for x in batch1[b'data'][i]]), batch1[b'labels'][i]] )
-
-    # Create KNN object where k = 3
-    K3 = KNearestNeighbor(batch1_f, 10)
 
     # Load test data and format
     test = unpickle("data/test_batch")
@@ -71,7 +70,10 @@ if __name__ == "__main__":
                         # Casting all values TO and int from uint8
         test_f.append( [numpy.array([int(x) for x in test[b'data'][i]]), test[b'labels'][i]] )
     
+    # Create KNN object
+    K3 = KNearestNeighbor(batch1_f)
+    
     print(test_f[0][1], end = " | ") # ith label
     print(test_f[0][0]) # list of rgb values [r, r, r, .... g, b, b]
 
-    print(f"test_f[0] classified as {K3.classify(test_f[0][0])}")
+    print(f"test_f[0] classified as {K3.classify(test_f[0][0], [1, 3, 10, 100, 1000])}")
